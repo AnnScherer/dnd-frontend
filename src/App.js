431d294da36c6c1c22f93d0323bf23b04ff4d51e
AnnScherer <ann.hardt@gmx.de>
2023-03-24 14:45:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import routes from "./components/routes";
+import UserContext from "./context/userContext";
+import { useState, useEffect, } from "react";
+import axios from "axios";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [spell, setSpell] = useState([]);
+  const [searchSpell, setSearchSpell] = useState([])
+  const [filteredSpells, setFilteredSpells] = useState([])
+  const [inhaltsverzeichnis, setInhaltsverzeichnis] = useState([])
+  useEffect(() => {
+    // GET request using axios inside useEffect React hook
+    axios
+      .get("http://localhost:3001/user/spells")
+      .then((response) => setSpell(response.data))
+      .catch((err) => console.log(err));
+
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, []);
+  console.log(isLoggedIn);
+  return (
+    <BrowserRouter>
+      <UserContext.Provider
+        value={{ setIsLoggedIn, isLoggedIn, spell, setSpell, searchSpell, setSearchSpell, filteredSpells, setFilteredSpells, inhaltsverzeichnis, setInhaltsverzeichnis }}
+      >
+        <Layout>
+          <Routes>
+            {routes.map((route) => {
+              return <Route key={route.id} {...route} />;
+            })}
+          </Routes>
+        </Layout>
+      </UserContext.Provider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
